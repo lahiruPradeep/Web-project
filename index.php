1b8fac9a2b55806a6065b1d152c97cb270242651
Ashan Himalka <ashan.rcm94@gmail.com>
2016-08-06 23:50:34 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test1</title>
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body class="body">
+	<header class="mainHeader">
+		<img src="nsbm.png" alt="nsbm">
+
+		<nav>
+			<ul>
+				<li><a href="#">Desktop</a></li>
+				<li><a href="#">Desktop</a></li>
+				<li><a href="#">Desktop</a></li>
+				<li><a href="#">Desktop</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<div class="mainContent">
+		<div class="content">
+		<article class="topContent">
+			<header>
+				<h2><a href="#" title="first post">first post</a></h2>
+			</header>
+
+			<footer>
+				<p class="info">this post is written by christian</p>
+			</footer>
+
+
+			<content>
+					<p>computinggfdgfdgvvrgvjsdiofodvisfedfggsd
+					sdgdsfdsgffdghgjhgfdsasdfghj,cvbnm,.hvcxswrgg
+					tgdhgfhhhdfgdsgfdsgfdgfgfdstrgdsftdgjhhgfhghdgsd
+					ggdhghfghdgfgdggjkjkfhtdgasrefaerhyjtgwewq</p>
+
+					<p>computinggfdgfdgvvrgvjsdiofodvisfedfggsd
+					sdgdsfdsgffdghgjhgfdsasdfghj,cvbnm,.hvcxswrgg
+					tgdhgfhhhdfgdsgfdsgfdgfgfdstrgdsftdgjhhgfhghdgsd
+					ggdhghfghdgfgdggjkjkfhtdgasrefaerhyjtgwewq</p>
+
+			</content>
+		</article>
+
+
+
+			<article class="bottomContent">
+			<header>
+				<h2><a href="#" title="first post">first post</a></h2>
+			</header>
+
+			<footer>
+				<p class="info">this post is written by christian</p>
+			</footer>
+
+
+			<content>
+					<p>computinggfdgfdgvvrgvjsdiofodvisfedfggsd
+					sdgdsfdsgffdghgjhgfdsasdfghj,cvbnm,.hvcxswrgg
+					tgdhgfhhhdfgdsgfdsgfdgfgfdstrgdsftdgjhhgfhghdgsd
+					ggdhghfghdgfgdggjkjkfhtdgasrefaerhyjtgwewq</p>
+
+					<p>computinggfdgfdgvvrgvjsdiofodvisfedfggsd
+					sdgdsfdsgffdghgjhgfdsasdfghj,cvbnm,.hvcxswrgg
+					tgdhgfhhhdfgdsgfdsgfdgfgfdstrgdsftdgjhhgfhghdgsd
+					ggdhghfghdgfgdggjkjkfhtdgasrefaerhyjtgwewq</p>
+
+			</content>
+		</article>
+		</div>
+	</div>
+
+	<aside class="top-sidebar">
+		<article>
+			<h2>top sidebar</h2>
+			<p>fhhhdfgdsgfdsgfdgfgfdstrgdsftdgjhhgfhghdgsd
+					ggdhghfghdgfgdggjkjkfhtdgasrefaerhyjt</p>
+		</article>
+	</aside>
+
+	<aside class="middle-sidebar">
+		<article>
+			<h2>middle sidebar</h2>
+			<p>fhhhdfgdsgfdsgfdgfgfdstrgdsftdgjhhgfhghdgsd
+					ggdhghfghdgfgdggjkjkfhtdgasrefaerhyjt</p>
+		</article>
+	</aside>
+
+	<aside class="bottom-sidebar">
+		<article>
+			<h2>bottomsidebar</h2>
+			<p>fhhhdfgdsgfdsgfdgfgfdstrgdsftdgjhhgfhghdgsd
+					ggdhghfghdgfgdggjkjkfhtdgasrefaerhyjt</p>
+		</article>
+	</aside>
+
+	<footer class="mainfooter">
+		<p>ashan himalka</p>
+	</footer>
+</body>
+</html>
